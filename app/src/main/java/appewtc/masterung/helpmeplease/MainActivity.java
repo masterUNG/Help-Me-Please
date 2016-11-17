ef6f1c5242d5535467aa7db89b64d67f2d6d33c5
masterUNG <phrombutr@gmail.com>
2016-11-17 15:08:32 +0700

    Map OK

--- app/src/main/java/appewtc/masterung/helpmeplease/MainActivity.java
+++ app/src/main/java/appewtc/masterung/helpmeplease/MainActivity.java
@@ -1,13 +1,37 @@
 package appewtc.masterung.helpmeplease;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button button;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        button = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.livFavorite);
+
+        //Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddValue.class));
+            }
+        });
+
+
+    }   // Main Method
+
+}   // Main Class
